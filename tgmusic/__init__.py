962afa88917e917338457fdce8ebfb26907fa4d5
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2023-12-11 20:49:59 +0530

    Update __init__.py

--- tgmusic/__init__.py
+++ tgmusic/__init__.py
@@ -2,6 +2,6 @@ from config.config import STRING, API_ID, API_HASH
 from pyrogram import Client
 from pytgcalls import PyTgCalls
 
-userbot = Client(STRING, api_id=config.API_ID, api_hash=config.API_HASH, plugins=dict(root="tgmusic"))
+userbot = Client(STRING, api_id=API_ID, api_hash=API_HASH, plugins=dict(root="tgmusic"))
 pytgcalls = PyTgCalls(userbot)
 
