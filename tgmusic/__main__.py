6886831da499aa6b6baebdd00c612e7e25921f69
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2023-12-11 20:39:44 +0530

    Update __main__.py

--- tgmusic/__main__.py
+++ tgmusic/__main__.py
@@ -1,6 +1,12 @@
-from tgmusic import pytgcalls
-
+from tgmusic import pytgcalls, userbot
+from pyrogram import idle
+from pytgcalls import idle as tgidle
 
 if __name__ == "__main__":
-    pytgcalls.run()
-
+   userbot.start()
+   print("UserBot Started")
+   pytgcalls.start()
+   print("Vc Client Started")
+   tgidle()
+   idle()
+    
